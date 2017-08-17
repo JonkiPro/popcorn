4c95fee6deb40f3c1ab3f6c184f79608638c7dd6
JonkiPro <slugocki.jonatan@gmail.com>
2017-08-17 16:01:23 +0200

    Revert "Revert "0.0.2.1""
    This reverts commit d9b8a68a57358ad7fab5e8af9b2e9801bbc61e55.

--- src/main/java/com/service/app/service/AuthorizationService.java
+++ src/main/java/com/service/app/service/AuthorizationService.java
@@ -1,6 +1,13 @@
 package com.service.app.service;
 
+/**
+ * The service works on authorized data.
+ */
 public interface AuthorizationService {
 
+    /**
+     * This method returns the user ID.
+     * @return Returns the user ID.
+     */
     Long getUserId();
 }
